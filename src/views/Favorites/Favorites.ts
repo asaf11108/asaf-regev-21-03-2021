206d6e2e1715fd78bdd88ddbce9e126556d35d18
asafHerolo <61415155+asafHerolo@users.noreply.github.com>
2021-03-31 19:23:25 +0300

    lint

--- src/views/Favorites/Favorites.ts
+++ src/views/Favorites/Favorites.ts
@@ -1,30 +1,40 @@
-import { ForecastData } from './../../components/Forecast/forecast.data';
-import { FavoriteLocation } from './../../store/favorite-location.interface';
-import Forecast from './../../components/Forecast/index.vue';
+import { ForecastData } from "./../../components/Forecast/forecast.data";
+import { FavoriteLocation } from "./../../store/favorite-location.interface";
+import Forecast from "./../../components/Forecast/index.vue";
 import { computed, defineComponent } from "vue";
-import { useRouter } from 'vue-router';
-import { useStore } from 'vuex';
+import { useRouter } from "vue-router";
+import { useStore } from "vuex";
 
 export default defineComponent({
-    name: 'Favorites',
-    components: {
-        Forecast
-    },
-    setup() {
-        const router = useRouter();
-        const store = useStore();
+  name: "Favorites",
+  components: {
+    Forecast,
+  },
+  setup() {
+    const router = useRouter();
+    const store = useStore();
 
-        const favoriteLocations = computed<FavoriteLocation[]>(() => (store.getters.selectEntities as FavoriteLocation[]).filter(favoriteLocation => favoriteLocation.isFavorite));
+    const favoriteLocations = computed<FavoriteLocation[]>(() =>
+      (store.getters.selectEntities as FavoriteLocation[]).filter(
+        (favoriteLocation) => favoriteLocation.isFavorite
+      )
+    );
 
-        const handleForecast = (favoriteLocation: FavoriteLocation): void => {
-            store.dispatch('setActive', favoriteLocation.id);
-            router.push({ name: 'Home' });
-        }
+    const handleForecast = (favoriteLocation: FavoriteLocation): void => {
+      store.dispatch("setActive", favoriteLocation.id);
+      router.push({ name: "Home" });
+    };
 
-        const mapToForecastComponent = (favoriteLocation: FavoriteLocation): ForecastData =>  {
-            return ({ title: favoriteLocation.locationName, temperature: favoriteLocation.temperature, icon: favoriteLocation.icon })
-        }
+    const mapToForecastComponent = (
+      favoriteLocation: FavoriteLocation
+    ): ForecastData => {
+      return {
+        title: favoriteLocation.locationName,
+        temperature: favoriteLocation.temperature,
+        icon: favoriteLocation.icon,
+      };
+    };
 
-        return { favoriteLocations, handleForecast, mapToForecastComponent };
-    }
+    return { favoriteLocations, handleForecast, mapToForecastComponent };
+  },
 });
